f2271390aad27d0e1f83dd687066f17d5289add6
Luís Lança <luis_lanca@outlook.com>
2024-06-12 21:56:26 -0300

    Update languages.js

--- src/constants/languages.js
+++ src/constants/languages.js
@@ -1,6 +1,5 @@
 export const LANGUAGES = [
   { label: "English", code: "en" },
   { label: "Português", code: "pt" },
-  { label: "Español", code: "es" },
-  { label: "Deutsch", code: "de" },
+  { label: "Español", code: "es" }
 ];
